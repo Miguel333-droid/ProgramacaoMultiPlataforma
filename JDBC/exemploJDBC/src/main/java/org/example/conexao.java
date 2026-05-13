79d3428e4f0e2d35a49fadafee5d93e2ff74648a
Miguel333-droid <miguelmarianooo07@gmail.com>
2026-05-13 11:05:47 -0300

    13/05

---
+++ JDBC/exemploJDBC/src/main/java/org/example/conexao.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexao {
+    public static Connection conectar(){
+        try{
+            return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/escola",
+                    "root",
+                    "mysqlfatec"
+            );
+        }catch (SQLException e ){
+            JOptionPane.showMessageDialog(null,"Erro na Conexão");
+            return null;
+        }
+    }
+
+}
